7fd2f9b8a05b55c62b6f5167ca740dc8b0990d9f
Jai Prakash <jai1.prakash@paytmmall.com>
2019-05-08 12:51:08 +0530

    description changes

--- src/main/java/com/practice/JumpGameMinJumps.java
+++ src/main/java/com/practice/JumpGameMinJumps.java
@@ -1,26 +1,21 @@
 package com.practice;
 
 /*
-https://leetcode.com/problems/jump-game/
-Jump Game
+https://leetcode.com/problems/jump-game-ii/
+Jump Game II
 
 Given an array of non-negative integers, you are initially positioned at the first index of the array.
 
 Each element in the array represents your maximum jump length at that position.
 
-Determine if you are able to reach the last index.
+Your goal is to reach the last index in the minimum number of jumps.
 
-Example 1:
+Example:
 
 Input: [2,3,1,1,4]
-Output: true
-Explanation: Jump 1 step from index 0 to 1, then 3 steps to the last index.
-Example 2:
-
-Input: [3,2,1,0,4]
-Output: false
-Explanation: You will always arrive at index 3 no matter what. Its maximum
-             jump length is 0, which makes it impossible to reach the last index.
+Output: 2
+Explanation: The minimum number of jumps to reach the last index is 2.
+    Jump 1 step from index 0 to 1, then 3 steps to the last index.
 
 */
 
